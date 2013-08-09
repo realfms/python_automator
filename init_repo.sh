375bc52229bc866bf2d5e7aa1f6e6552cc58fe5e
EC2 Default User <ec2-user@ip-10-252-1-19.us-west-2.compute.internal>
2013-08-09 09:06:44 +0000

    Changed init_repo.sh to download from github

--- init_repo.sh
+++ init_repo.sh
@@ -6,5 +6,4 @@ mkdir backoffice_process_manager
 cd backoffice_process_manager
 virtualenv -p /usr/bin/python2.7 venv --distribute
 
-#git clone https://github.com/PDI-DGS-Protolab/backoffice_process_manager
-cp -rf $HOME/code backoffice_process_manager
+git clone https://github.com/PDI-DGS-Protolab/backoffice_process_manager
