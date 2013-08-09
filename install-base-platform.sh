893ccea4d848e18fef99debca8e06c8c676bac14
Miguel Angel Cañas Vaz <macvaz82@gmail.com>
2013-08-09 13:48:02 +0200

    Now installing security updates

--- install-base-platform.sh
+++ install-base-platform.sh
@@ -23,4 +23,10 @@ git clone https://github.com/PDI-DGS-Protolab/python_automator
 
 echo 'export PATH=$PATH:$HOME/python_automator/' >> $HOME/.bashrc
 
+echo  "Installing security updates!"
+
+sudo yum -y update
+
 echo  "Installation FINISHED!"
+
+source .bashrc
