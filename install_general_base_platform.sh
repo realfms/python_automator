3ae87204e1d67d3f75fb093977914a28b06f352a
Miguel Angel Cañas Vaz <macvaz82@gmail.com>
2013-08-09 12:59:42 +0200

    Adding foreman dependencies

--- install_general_base_platform.sh
+++ install_general_base_platform.sh
@@ -3,4 +3,14 @@
 sudo yum -y install git, python27, python-pip
 sudo easy_install virtualenv
 
+sudo yum erase -y ruby rubygems
+sudo yum -y install ruby19
+
+yum erase -y ruby rubygems
+
+sudo ln -s /usr/bin/gem1.9 /usr/bin/gem
+sudo ln -s /usr/bin/ruby1.9 /usr/bin/ruby
+
+sudo gem install foreman
+
 echo 'alias pip="/usr/bin/pip-python"' >> $HOME/.bashrc
