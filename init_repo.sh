cd8bab78c0b6ee95c725631e0289412eb32ddc64
EC2 Default User <ec2-user@ip-10-252-85-95.us-west-2.compute.internal>
2013-08-09 10:11:22 +0000

    Fixed problems creating repo

--- init_repo.sh
+++ init_repo.sh
@@ -6,4 +6,10 @@ mkdir backoffice_process_manager
 cd backoffice_process_manager
 virtualenv -p /usr/bin/python2.7 venv --distribute
 
+source venv/bin/activate
+
 git clone https://github.com/PDI-DGS-Protolab/backoffice_process_manager
+
+cd backoffice_process_manager
+
+git checkout automator
--- update_code.sh
+++ update_code.sh
@@ -4,5 +4,12 @@ cd $HOME
 cd backoffice_process_manager
 
 source venv/bin/activate
+
+git pull origin automator
+
+echo "Updating base platform dependencies"
+sh ./backoffice_process_manager/platform-installation-script.sh
+
+echo "Updating python dependencies"
 pip install -r backoffice_process_manager/requirements.txt
 
